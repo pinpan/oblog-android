f675b2a00f83ed45db29a9d912787f21e54a1b01
pavlin <pavlin@email.cz>
2020-03-10 08:02:22 +0100

    WIP: Detaisl tabs

---
+++ app/src/main/java/com/applego/oblog/tppwatch/TppDetailTabsFragment.kt
@@ -0,0 +1,30 @@
+package com.applego.oblog.tppwatch
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class TppDetailTabsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = TppDetailTabsFragment()
+    }
+
+    private lateinit var viewModel: TppDetailTabsViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.tpp_detail_tabs_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(TppDetailTabsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/applego/oblog/tppwatch/TppDetailTabsViewModel.kt
@@ -0,0 +1,7 @@
+package com.applego.oblog.tppwatch
+
+import androidx.lifecycle.ViewModel
+
+class TppDetailTabsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
